834cdbaf9ef86c47b0d3eb75cc5ea7b6b8188e1f
IvanConsalter <ivanconsalter1@gmail.com>
2020-09-16 13:42:06 -0300

    smooth control

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
+const navBtn = document.querySelector('#nav-toggle')
+const links = document.querySelector('#links')
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
 /**
  * TOGGLE BUTTON
  */
-document.querySelector('#nav-toggle').addEventListener('click', () => {
-    document.querySelector('#links').classList.toggle('show-links')
+navBtn.addEventListener('click', () => {
+    links.classList.toggle('show-links')
 })
 
+/**
+ * SMOOTH SCROLL
+ */
+scrollLinks.forEach( link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        links.classList.remove('show-links')
+
+        const id = event.target.getAttribute('href').slice(1)
+        const element = document.querySelector(`#${id}`)
+
+        let position = element.offsetTop - 71
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: 'smooth'
+        })
+    })
+})
